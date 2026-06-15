837401f034bded8b4978be8d171a5a43f545d3cd
jennychaux-eng <jenny.chaux@uao.edu.co>
2026-06-15 17:53:58 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -78,7 +78,7 @@ def login_page():
     box-shadow: 0 12px 40px rgba(0,0,0,0.18);
     /* larger rectangular background on the right */
     position: absolute;
-    right: 1%;
+    right: calc(1% - 2px);
     top: 10%;
     max-width: 2500px;
     min-height: 500px;
