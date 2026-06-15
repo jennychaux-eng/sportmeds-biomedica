2d41f79a112d091644c03e94b8f3a6877edacf73
jennychaux-eng <jenny.chaux@uao.edu.co>
2026-06-15 17:49:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -80,7 +80,7 @@ def login_page():
     position: absolute;
     right: 0%;
     top: 8%;
-    max-width: 1800px;
+    max-width: 2000px;
     min-height: 300px;
     width: 100%;
     box-sizing: border-box;
