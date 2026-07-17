a8413802da1515d435e05392e99595d8ec08b7fa
jennychaux-eng <jenny.chaux@uao.edu.co>
2026-07-16 19:35:52 -0500

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,1 +1,41 @@
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+import os
+import base64
+import streamlit as st
+
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+from supabase import create_client, Client
+
+BASE_DIR = os.path.dirname(
+    os.path.dirname(os.path.abspath(__file__))
+)
+
+LOGO_PATH = os.path.join(
+    BASE_DIR,
+    "assets",
+    "Logo_sportmeds.png"
+)
+
+
+def get_fecha_local():
+    return datetime.now(
+        ZoneInfo("America/Bogota")
+    ).date()
+
+
+def get_base64_image(path):
+    with open(path, "rb") as img:
+        return base64.b64encode(
+            img.read()
+        ).decode()
+
+
+@st.cache_resource
+def init_supabase():
+    url = st.secrets["SUPABASE_URL"]
+    key = st.secrets["SUPABASE_KEY"]
+    return create_client(url, key)
+
+
+supabase: Client = init_supabase()
