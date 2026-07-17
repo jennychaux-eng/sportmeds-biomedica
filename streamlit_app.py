afe498200fae382f4ad68f3c8b5543b42add2ec5
jennychaux-eng <jenny.chaux@uao.edu.co>
2026-07-16 19:38:19 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,39 +6,6 @@ from config.config import (
     get_base64_image
 )
 
-import streamlit as st
-import os
-import pandas as pd
-import plotly.graph_objects as go
-import base64
-from datetime import date, datetime, timedelta
-from zoneinfo import ZoneInfo
-from supabase import create_client, Client
-
-
-def get_fecha_local():
-    return datetime.now(ZoneInfo("America/Bogota")).date()
-
-def get_base64_image(path):
-    with open(path, "rb") as img:
-        return base64.b64encode(img.read()).decode()
-        
-# ─────────────────────────────────────────
-# RUTAS
-# ─────────────────────────────────────────
-BASE_DIR  = os.path.dirname(os.path.abspath(__file__))
-LOGO_PATH = os.path.join(BASE_DIR, "assets", "Logo_sportmeds.png")
-
-# ─────────────────────────────────────────
-# CONEXIÓN SUPABASE
-# ─────────────────────────────────────────
-@st.cache_resource
-def init_supabase():
-    url = st.secrets["SUPABASE_URL"]
-    key = st.secrets["SUPABASE_KEY"]
-    return create_client(url, key)
-
-supabase: Client = init_supabase()
 # ==========================================
 # AUTENTICACIÓN
 # ==========================================
