adcf6127316fb383d644c88365a4622585d7e702
jennychaux-eng <jenny.chaux@uao.edu.co>
2026-07-16 20:00:17 -0500

    Update topbar.py

--- components/topbar.py
+++ components/topbar.py
@@ -1,1 +1,12 @@
+import streamlit as st
 
+def topbar(titulo, ruta):
+    st.markdown(f"""
+    <div class="topbar">
+        <div>
+            <div class="topbar-title">{titulo}</div>
+            <div class="topbar-crumb">INICIO › {ruta}</div>
+        </div>
+        <div class="topbar-user">👤 &nbsp; </div>
+    </div>
+    """, unsafe_allow_html=True)
